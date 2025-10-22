eac04f2847152a54b446ba8704c8c373ed9c0f8a
Jose Antonio González Alcántara <jagascripts@gmail.com>
2025-10-22 10:28:32 +0200

    Update database URL in configuration and refactor fill_db script for improved logging and error handling

--- app/core/database.py
+++ app/core/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./fakestore.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./app/core/fakestore.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
