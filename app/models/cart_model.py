5b017ebb50355bf5dceccd103449e9b759237114
Jose Antonio González Alcántara <jagascripts@gmail.com>
2025-10-24 13:53:43 +0200

    Refactor product and cart models and schemas to standardize naming conventions

--- app/models/cart_model.py
+++ app/models/cart_model.py
@@ -1,14 +1,14 @@
 # app/models/cart_model.py
 
-from sqlalchemy import Column, Integer, String, DateTime, JSON, ForeignKey
+from sqlalchemy import Column, Integer, DateTime, JSON, ForeignKey
 from sqlalchemy.orm import relationship
 from .dec_base import DecBase
 
 class CartItem(DecBase):
     __tablename__ = "cart_items"
 
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("users.id"))
+    id = Column(Integer, primary_key=True, index=True, autoincrement=False)
+    userId = Column(Integer, ForeignKey("users.id"))
     date = Column(DateTime)
     products = Column(JSON)
     
